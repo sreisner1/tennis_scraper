8b9e422cbe8fb242fe3ef3a32e3558f594b1993a
sreisner1 <98129421+sreisner1@users.noreply.github.com>
2023-08-15 10:20:16 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,4 @@
-
- import time
+import time
 from datetime import datetime
 
 from selenium import webdriver
@@ -15,46 +14,53 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 import csv
 import text_dad
 
+
 def open_file():
-    file = open('/Users/samreisner/Desktop/Tennis_Scraper/courts.csv') 
+    file = open('/Users/samreisner/Desktop/Tennis_Scraper/courts.csv', 'w', newline='') 
+
     writer = csv.writer(file)
     return writer
-    
+
+def is_empty_file():
+    with open('/Users/samreisner/Desktop/Tennis_Scraper/courts.csv', 'r') as file:
+        return not any(csv.reader(file))  # Check if any rows exist  
+      
 def read_file():
-    all_courts = []
-    file = open_file()
+    if is_empty_file():
+        return []
+    file = open('/Users/samreisner/Desktop/Tennis_Scraper/courts.csv') 
     csvreader = csv.reader(file)
-    next(csvreader)
+    all_courts = []
     for row in csvreader:
         all_courts.append(row)
     file.close()
     return all_courts
 
 def write_to_file(data):
-    file = open_file()
+    writer = open_file()
     for row in data:
-        file.writerow(row)    
-    file.close()
+        print(row)
+        writer.writerow(row)    
 
 def delete_file():
     f = open('/Users/samreisner/Desktop/Tennis_Scraper/courts.csv', "w")
     f.truncate()
     f.close()
 
-def all_openings(court_data, courts, times, day):
+def all_openings(court_data, courts, times):
     all_openings = []
     for i in range(court_data):
         time = str(int(times[i].split(':')[0]))
         court = str(int(courts[i]))
-        lst = [time, court, day]
+        lst = [time, court]
         all_openings.append(lst)
     return all_openings
  
-def send_messages(court_data, courts, times, day):
-    for i in range(court_data):
-        time = str(int(times[i].split(':')[0]))
-        court = courts[i]
-        message = "Court " + court + " is available " + day +  " at " + time
+def send_messages(court_data):
+    for i in range(len(court_data)):
+        time = court_data[i][0]
+        court = court_data[i][1]
+        message = "Court " + court + " is available today at " + time
         print(message)
         text_dad.send_message(message)
 
@@ -73,6 +79,11 @@ def get_current_hour():
     if current_minute < 15:
         return current_hour - 1
     return current_hour
+
+def after_seven():
+    curr_hour = get_current_hour()
+    if curr_hour >= 19:
+        delete_file()
   
 
 def get_starting_row(hour):
@@ -118,7 +129,6 @@ def get_today_sheet(driver):
     display_button.click()
     time.sleep(1)
     return driver
-  
 def get_tomorrow_sheet(driver):
     choose_tomorrow = driver.find_element(By.XPATH, '//*[@id="ddlPlaydate"]/option[2]')
     choose_tomorrow.click()
@@ -127,52 +137,52 @@ def get_tomorrow_sheet(driver):
     display_button.click()
     time.sleep(1)
     return driver
-
 def scrape_table(driver, starting_row):
     courts_found = 0
     court_time = []
     courts = []
+    # Find the table element using its tag name ('table')
+    table = driver.find_element(By.TAG_NAME, 'table')
+
+    # Find all the rows within the table using the 'tr' tag
+    rows = table.find_elements(By.TAG_NAME, 'tr')
+
+    # The number of rows is the length of the 'rows' list
+    num_rows = len(rows)
+
     row = 2 + starting_row
-    name = 4
-    #68 rows in the whole table
-    while row < 40:
+
+    while row < num_rows + 1:
         xpath = "//*[@id='GridView2']/tbody/tr[{}]/td[4]".format(row)
         row_data = driver.find_element(By.XPATH, xpath)
         #print(row_data.text)
-        if len(row_data.text) > 1:
-            time.sleep(1)
-        else:
+        if row_data.text == " " or row_data.text == None:
             xpath = "//*[@id='GridView2']/tbody/tr[{}]/td[1]".format(row)
             court_time.append(driver.find_element(By.XPATH, xpath).text)
             xpath = "//*[@id='GridView2']/tbody/tr[{}]/td[3]".format(row)
             courts.append(driver.find_element(By.XPATH, xpath).text)
             courts_found += 1
+        else:
+            time.sleep(1)
         row += 1
     return courts_found, court_time, courts
 
 def main():
+    #csv file is structed as: time, court
+    after_seven()
     hour = get_current_hour()
+  
     starting = get_starting_row(hour)
     #every hour is +6 rows (except 5 & 6)
     driver = get_to_court_sheet()
-    #driver = get_today_sheet(driver)
-    #courts_found_today, times_today = scrape_table(driver, starting)
-    get_tomorrow_sheet(driver)
-    courts_found_tom, times_tom, courts_tom = scrape_table(driver, 0)
-
+    driver = get_today_sheet(driver)
+    num_courts_found, times, courts_today = scrape_table(driver, starting)
     sent_courts = read_file()
-    #all_openings_today = all_openings(courts_found_t, courts, times_tom, 'today')
-
-    all_openings_tomorrow = all_openings(courts_found_tom, courts_tom, times_tom, 'tomorrow')
-    #all_openings_today = all_openings(courts_found_t, courts, times_tom, 'today')
-
-    print(sent_courts, all_openings_tomorrow)
-    courts_to_send = find_unique(sent_courts, all_openings_tomorrow)
-    print(courts_to_send)
-    #delete_file()
+    all_openings_today = all_openings(num_courts_found, courts_today, times)
+    courts_to_send = find_unique(sent_courts, all_openings_today)
+ 
+    send_messages(courts_to_send)
     write_to_file(courts_to_send)
     
-    #after 7, everyday erase the file
-
 if __name__ == "__main__":
     main()
