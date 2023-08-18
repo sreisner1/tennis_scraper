b45aaf84ef588e2aaf2bea6571ec38312806d3cf
sreisner1 <98129421+sreisner1@users.noreply.github.com>
2023-08-18 12:52:49 -0400

    Create run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+osascript -e 'do shell script "/usr/local/bin/python3 /Users/samreisner/Desktop/Tennis_Scraper/scraper.py >> /Users/samreisner/Desktop/Tennis_Scraper/logfile.log 2>&1"'
